e342b4dfc95ce63aa16c4045b820e4c29556bd33
Valeriy95 <vmariukhnyk@gmail.com>
2025-01-06 17:44:26 +0300

    feat: add style

--- src/components/GallowsPart.tsx
+++ src/components/GallowsPart.tsx
@@ -4,13 +4,15 @@ const GallowsPart: React.FC = () => {
   return (
     <div className='gallows-part-container'>
         <h1 className='title'>HANGMAN GAME</h1>
-        <div className='img-gallows'></div>
-        <div className='img-head'></div>
-        <div className='img-body'></div>
-        <div className='img-hand-one'></div>
-        <div className='img-hand-two'></div>
-        <div className='img-leg-one'></div>
-        <div className='img-leg-two'></div>
+        <div className='gallows-part-content'>
+          <div className='img-gallows'></div>
+          <div className='img-head'></div>
+          <div className='img-body'></div>
+          <div className='img-hand-one'></div>
+          <div className='img-hand-two'></div>
+          <div className='img-leg-one'></div>
+          <div className='img-leg-two'></div>
+        </div>
     </div>
   );
 }
